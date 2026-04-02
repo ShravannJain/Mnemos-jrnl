8ac92e8cd1d5e199273c6355491b91f262c2c8d8
shravan jain <shravanjain3065@gmail.com>
2026-04-02 21:47:29 +0530

    chore: initialize Spring Boot project

---
+++ mnemos/src/main/java/com/shravan/mnemos/MnemosApplication.java
@@ -0,0 +1,14 @@
+package com.shravan.mnemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnemosApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
